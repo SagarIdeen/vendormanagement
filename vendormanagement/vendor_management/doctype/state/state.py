bb2631860812e0237f1f7711ab4c507fd3d0ed4a
Saluideen <isaluideen@gmail.com>
2023-10-17 20:09:24 +0530

    gst validation

--- vendormanagement/vendor_management/doctype/state/state.py
+++ vendormanagement/vendor_management/doctype/state/state.py
@@ -1,13 +1,15 @@
 # Copyright (c) 2023, Ideenkreice and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class State(Document):
 	pass
 
-@frappe.whitelist()
+@frappe.whitelist(allow_guest=True)
 def get_gst_code_for_state(state_name):
+	print("gst",state_name)
 	data=frappe.db.sql("""select gst_code from `tabState` where state=%(state)s """,values={'state':state_name},as_dict=1)
+	print("gst",data)
 	return data
--- vendormanagement/vendor_management/web_form/vendor_details/vendor_details.js
+++ vendormanagement/vendor_management/web_form/vendor_details/vendor_details.js
@@ -1,88 +1,64 @@
 frappe.ready(function() {
-    // Bind events here
-    frappe.web_form.validate = () => {
-        let data = frappe.web_form.get_values();
-		// Validation for Pan Number
-        let pan_number = data.pan_number;
-		if (pan_number){
-			 // Replace the pattern with the correct format for PAN numbers
-			 let panNumberPattern = /^[A-Z]{5}[0-9]{4}[A-Z]$/;
-
-			 // Check if the entered PAN number matches the pattern
-			 if (!panNumberPattern.test(pan_number)) {
-				 frappe.msgprint(__("Invalid PAN Number. Please enter a valid PAN Number."));
-				 return false; 
-			 }
-			 return true;
-
-		}
-
-		// GST Validation
-		let gst_number = data.gst_number;
-		const stateName = data.state; // Assuming state is a field on your web form
-        const gstCode =  getGSTCodeForState(stateName);
-		if(gst_number && pan_number){
-			let gst_pan_portion = gst_number.substr(2, 5);
-			// Check if the extracted GST PAN portion matches the entered PAN number
-            if (gst_pan_portion !== pan_number) {
-                frappe.msgprint(__("The PAN number does not match the GST number. Please enter a matching PAN number."));
-                return false; 
-            }
-			// Replace the pattern with the correct format for GST numbers
-			let gstNumberPattern = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}[Z]{1}[0-9]{1}$/;
-
-			if (!gstNumberPattern.test(gst_number)) {
-				frappe.msgprint(__("Invalid GST Number. Please enter a valid GST Number."));
-				return false; 
-			}
-	
-			
-			return true;
-
-		}
-		if(gst_number && pan_number){
-			let gst_pan_portion = gst_number.substr(2, 5);
-			// Check if the extracted GST PAN portion matches the entered PAN number
-            if (gst_pan_portion !== pan_number) {
-                frappe.msgprint(__("The PAN number does not match the GST number. Please enter a matching PAN number."));
-                return false; 
-            }
-			// Replace the pattern with the correct format for GST numbers
-			let gstNumberPattern = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}[Z]{1}[0-9]{1}$/;
-
-			if (!gstNumberPattern.test(gst_number)) {
-				frappe.msgprint(__("Invalid GST Number. Please enter a valid GST Number."));
-				return false; 
-			}
-	
-			
-			return true;
-
-		}
-
+    frappe.web_form.validate = async () => {
+        const data = frappe.web_form.get_values();
+        const pan_number = data.pan_number;
+        const gst_number = data.gst_provisional_id;
+        const stateName = data.state;
         
-		
-       
+        // PAN Number Validation
+        if (pan_number) {
+            const panNumberPattern = /^[A-Z]{5}[0-9]{4}[A-Z]$/;
+            if (!panNumberPattern.test(pan_number)) {
+                frappe.msgprint(__("Invalid PAN Number. Please enter a valid PAN Number."));
+                return false;
+            }
+        }
+        
+        // GST Validation
+        if (gst_number) {
+            let gstCodes = await getGSTCodeForState(stateName);
+			console.log("gst_code",gstCodes);
+            if (gstCodes) {
+                
+                let gstCodePortion = gst_number.substr(0, 2); // Extract first 2 characters as GST code
+   				 let panPortion = gst_number.substr(2, 10); // Extract next 10 characters as PAN number
+    			let gstNumberPattern = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z][1-9A-Z]Z[0-9]$/;
+
+
+                if (gstCodePortion !== gstCodes || panPortion !== pan_number || !gstNumberPattern.test(gst_number)) {
+					console.log("gst_code1",gstCodes);
+                    frappe.msgprint(__("Invalid GST Number. Please enter a valid GST Number."));
+                    return false;
+                }
+            } else {
+                frappe.msgprint(__("No GST code found for the selected state."));
+                return false;
+            }
+        }
     };
-	async function getGSTCodeForState(stateName) {
-		try {
-			const response = await frappe.call({
-				method: "vendormanagement.vendor_management.doctype.state.state.get_gst_code_for_state",
-				args: { state_name: stateName },
-			});
+
+	function getGSTCodeForState(stateName) {
+        return new Promise((resolve, reject) => {
+            frappe.call({
+                method:
+                    "vendormanagement.vendor_management.doctype.state.state.get_gst_code_for_state",
+                args: {
+                    state_name: stateName,
+                },
+                freeze: true,
+                callback: (r) => {
+                    if (r.message && r.message[0] && r.message[0].gst_code) {
+                        resolve(r.message[0].gst_code);
+                    } else {
+                        resolve(null);
+                    }
+                },
+                error: (r) => {
+                    reject(r);
+                },
+            });
+        });
+    }
 	
-			if (response.message) {
-				return response.message;
-			} else {
-				return null;
-			}
-		} catch (error) {
-			frappe.msgprint(__("An error occurred while fetching the GST code."));
-			return null;
-		}
-	}
-		
 	
 });
-
-
