64d273c47366d0fd7fe94795fdf3264378668039
Saluideen <isaluideen@gmail.com>
2023-10-20 17:12:06 +0530

    city,country,state data imported

--- vendormanagement/vendor_management/doctype/vendor_city/vendor_city_list.js
+++ vendormanagement/vendor_management/doctype/vendor_city/vendor_city_list.js
@@ -1,19 +1,19 @@
-frappe.listview_settings['Vendor City'] = {
-    onload: function (listview) {
-        // Add a custom button to the list view toolbar
-        listview.page.add_button(__("Sync"), function () {
-            frappe.call({
-                method: "vendormanagement.vendor_management.doctype.vendor_city.vendor_city.get_city",
-                freeze: true,
-                callback: function (r) {
-                    console.log(r.message);
+// frappe.listview_settings['Vendor City'] = {
+//     onload: function (listview) {
+//         // Add a custom button to the list view toolbar
+//         listview.page.add_button(__("Sync"), function () {
+//             frappe.call({
+//                 method: "vendormanagement.vendor_management.doctype.vendor_city.vendor_city.get_city",
+//                 freeze: true,
+//                 callback: function (r) {
+//                     console.log(r.message);
                    
                     
-                },
-                error: function (r) {
-                    frappe.msgprint(r.message);
-                },
-            });
-        });
-    }
-};
+//                 },
+//                 error: function (r) {
+//                     frappe.msgprint(r.message);
+//                 },
+//             });
+//         });
+//     }
+// };
