3df5ed655577c6cf4209b0762e2917d293f8f7b3
Saluideen <isaluideen@gmail.com>
2023-10-28 15:17:45 +0530

    list_view_change

--- vendormanagement/vendor_management/doctype/vendor_details/vendor_details_list.js
+++ vendormanagement/vendor_management/doctype/vendor_details/vendor_details_list.js
@@ -1,19 +1,27 @@
 frappe.listview_settings['Vendor Details'] = {
     onload: function (listview) {
         // Add a custom button to the list view toolbar
-        listview.page.add_button(__("Sync"), function () {
-            frappe.call({
-                method: "vendormanagement.vendor_management.doctype.vendor_details.vendor_details.get_vendor_data",
-                freeze: true,
-                callback: function (r) {
-                    console.log(r.message);
+        // listview.page.add_button(__("Sync"), function () {
+        //     frappe.call({
+        //         method: "vendormanagement.vendor_management.doctype.vendor_details.vendor_details.get_vendor_data",
+        //         freeze: true,
+        //         callback: function (r) {
+        //             console.log(r.message);
                    
                     
-                },
-                error: function (r) {
-                    frappe.msgprint(r.message);
-                },
+        //         },
+        //         error: function (r) {
+        //             frappe.msgprint(r.message);
+        //         },
+        //     });
+        // });
+        listview.page.set_secondary_action(__("Sync "), function(frm) {
+
+            frappe.call({
+                method:"vendormanagement.vendor_management.doctype.vendor_details.vendor_details.get_vendor_data", 
+                args:{},
             });
+        
         });
     }
 };
