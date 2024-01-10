d19d79ae0f3aa225c12eed4db98b935ec3dd9dfe
Saluideen <isaluideen@gmail.com>
2024-01-10 14:58:14 +0530

    deleted

--- vendormanagement/vendor_management/doctype/vendor_demo_city/test_vendor_demo_city.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Ideenkreice and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class Testvendor_demo_city(FrappeTestCase):
-	pass
